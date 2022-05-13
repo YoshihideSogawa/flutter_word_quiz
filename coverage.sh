d2f45e6be63e6ffc05d7e2d8fe956dc8631dcb79
Yoshihide Sogawa <12043975+YoshihideSogawa@users.noreply.github.com>
2022-05-13 19:06:06 +0900

    「ignore: avoid_classes_with_only_static_members」の削除 (#2)
    * ignoreを削除
    
    * Coverageのignore
    
    * coverage.shのリファクタリング

--- coverage.sh
+++ coverage.sh
@@ -1,20 +1,23 @@
 # https://qiita.com/gki/items/a861d7ac1c61d6de928e
 # https://github.com/priezz/dart_full_coverage
-#!/bin/sh
+#/bin/sh
 
 outputFile="$(pwd)/test/coverage_test.dart"
-packageName="$(cat pubspec.yaml| grep '^name: ' | awk '{print $2}')"
+packageName="$(< pubspec.yaml grep '^name: ' | awk '{print $2}')"
 
 if [ "$packageName" = "" ]; then
     echo "Run $0 from the root of your Dart/Flutter project"
     exit 1
 fi
 
-echo "/// *** GENERATED FILE - ANY CHANGES WOULD BE OBSOLETE ON NEXT GENERATION *** ///\n" > "$outputFile"
-echo "/// Helper to test coverage for all project files" >> "$outputFile"
-echo "// ignore_for_file: unused_import" >> "$outputFile"
-find lib -name '*.dart' | grep -v '.g.dart' | grep -v '.freezed.dart' | grep -v 'generated_plugin_registrant' | awk -v package=$packageName '{gsub("^lib", "", $1); printf("import '\''package:%s%s'\'';\n", package, $1);}' >> "$outputFile"
-echo "\nvoid main() {}" >> "$outputFile"
+printf "/// *** GENERATED FILE - ANY CHANGES WOULD BE OBSOLETE ON NEXT GENERATION *** ///\n" > "$outputFile"
+
+{
+  printf "/// Helper to test coverage for all project files\n"
+  printf "// ignore_for_file: unused_import\n"
+  find lib -name '*.dart' | grep -v '.g.dart' | grep -v '.freezed.dart' | grep -v 'generated_plugin_registrant' | awk -v package="$packageName" '{gsub("^lib", "", $1); printf("import '\''package:%s%s'\'';\n", package, $1);}'
+  printf "\nvoid main() {}"
+} >> "$outputFile"
 
 flutter test --coverage
 
@@ -22,7 +25,7 @@ lcov --remove coverage/lcov.info '**/*.g.dart' '**/*.freezed.dart' -o coverage/l
 
 genhtml coverage/lcov.info -o coverage/result
 
-rm $outputFile
+rm "$outputFile"
 
 # open coverage html
 # open coverage/result/index.html
