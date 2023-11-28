bc03d9b12b1c53492307baf7da86b8b9dc52ee98
YoshihideSogawa <12043975+YoshihideSogawa@users.noreply.github.com>
2023-11-28 18:20:16 +0900

    テスト分割

--- test/repository/quiz/statistics_repository_test.dart
+++ test/repository/quiz/statistics_repository_test.dart
@@ -1,65 +1,13 @@
-// TODO(sogawa): 分割したクラスに応じてテストを分割する
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:word_quiz/constant/box_names.dart';
 import 'package:word_quiz/model/quiz_statistics.dart';
 import 'package:word_quiz/model/quiz_type.dart';
-import 'package:word_quiz/model/word_input.dart';
-import 'package:word_quiz/model/word_keyboard_state.dart';
-import 'package:word_quiz/model/word_name_state.dart';
 import 'package:word_quiz/repository/quiz/quiz_data_repository.dart';
 import 'package:word_quiz/repository/quiz/statistics_repository.dart';
-import 'package:word_quiz/repository/quiz/word_input_repository.dart';
 
-import '../mock/hive_tester.dart';
-import '../mock/mock_box_data.dart';
+import '../../mock/mock_box_data.dart';
 
 void main() {
-  setUp(() async {
-    setUpHive();
-    await Hive.openBox<dynamic>(dailyBoxName);
-    await Hive.openBox<dynamic>(endlessBoxName);
-  });
-
-  tearDown(tearDownHive);
-
-  test('WordInput(save/load)', () async {
-    const quizType = QuizTypes.endless;
-    final container = ProviderContainer();
-
-    const wordInput = WordInput(
-      wordsList: [
-        ['テ', 'ス', 'ト'],
-      ],
-      wordsResultList: [
-        [WordNameState.notMatch, WordNameState.hit, WordNameState.match],
-      ],
-      keyResultList: {
-        'テ': WordKeyboardInfo.notMatch,
-        'ス': WordKeyboardInfo.hit,
-        'ト': WordKeyboardInfo.match,
-      },
-      inputIndex: 1,
-      isWordChecking: true,
-    );
-
-    // 保存
-    await container
-        .read(wordInputRepositoryProvider(quizType).notifier)
-        .saveWordInput(wordInput);
-
-    // 読み込み
-    final targetWordInfo =
-        await container.read(wordInputRepositoryProvider(quizType).future);
-
-    expect(targetWordInfo!.wordsList, wordInput.wordsList);
-    expect(targetWordInfo.wordsResultList, wordInput.wordsResultList);
-    expect(targetWordInfo.keyResultList, wordInput.keyResultList);
-    expect(targetWordInfo.inputIndex, wordInput.inputIndex);
-    expect(targetWordInfo.isWordChecking, wordInput.isWordChecking);
-  });
-
   test('QuizStatistics(save/load)', () async {
     const quizType = QuizTypes.endless;
     final container = ProviderContainer(
