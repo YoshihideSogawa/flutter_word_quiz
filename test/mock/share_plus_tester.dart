a7d0176f35eec7d5f67a0f59d7ea6e7097c3a6ae
YoshihideSogawa <12043975+YoshihideSogawa@users.noreply.github.com>
2024-12-31 16:56:42 +0900

    パッケージの更新

--- test/mock/share_plus_tester.dart
+++ test/mock/share_plus_tester.dart
@@ -15,51 +15,13 @@ class FakeSharePlus extends Fake
     implements SharePlatform {
   bool shareCalled = false;
 
-  bool shareFilesCalled = false;
-
-  bool shareFilesWithResultCalled = false;
-
-  bool shareWithResultCalled = false;
-
   @override
-  Future<void> share(
+  Future<ShareResult> share(
     String text, {
     String? subject,
     Rect? sharePositionOrigin,
   }) async {
     shareCalled = true;
-  }
-
-  @override
-  Future<void> shareFiles(
-    List<String> paths, {
-    List<String>? mimeTypes,
-    String? subject,
-    String? text,
-    Rect? sharePositionOrigin,
-  }) async {
-    shareFilesCalled = true;
-  }
-
-  @override
-  Future<ShareResult> shareFilesWithResult(
-    List<String> paths, {
-    List<String>? mimeTypes,
-    String? subject,
-    String? text,
-    Rect? sharePositionOrigin,
-  }) async {
-    shareFilesWithResultCalled = true;
-    return const ShareResult('success', ShareResultStatus.success);
-  }
-
-  @override
-  Future<ShareResult> shareWithResult(
-    String text, {
-    String? subject,
-    Rect? sharePositionOrigin,
-  }) async {
-    shareWithResultCalled = true;
     return const ShareResult('success', ShareResultStatus.success);
   }
 }
